751cbfe5345c18898f617e9e1dcd7c5674e27363
AkashMuraliNEW <akashmurali013@gmail.com>
2025-07-30 22:16:07 +0530

    update

---
+++ first_program.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int a = 50,b=40,c=10;
+
+void func() {
+  cout << "Hello World!" << '\n';
+  
+}
+int main () {
+  func();
+  cout << a + b + c;
+  return 0;
+}
