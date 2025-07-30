8d601d02188c6baecaf57ae603248bfda784f4f7
AkashMuraliNEW <akashmurali013@gmail.com>
2025-07-30 22:56:19 +0530

    fewtHings

--- programs/first_program.cpp
+++ programs/first_program.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
 int a = 50,b=40,c=10;
+int x;
+
 
 void func() {
   cout << "Hello World!" << '\n';
-
+  cin >> x;
 }
 int main () {
   func();
-  cout << a + b + c;
+  
+  cout << "sum of numbers : " <<  a + b + c << '\n' << "input number is " << x ;
   return 0;
 }
