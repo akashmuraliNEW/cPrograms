6c4f4513045283d4fb4dd0395b2740fcd889b46f
AkashMuraliNEW <akashmurali013@gmail.com>
2025-08-01 19:34:05 +0530

    opera

---
+++ programs/operations.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int x=1, y=2;
+int main(){
+    int sum = x+y;
+    int multy = x*y;
+    cout << "sum of the number : " << sum << "\n multy of nums : " << multy ;
+
+}
