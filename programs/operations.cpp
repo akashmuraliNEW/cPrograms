45832abd016562c1faad540e8e204b9393cee496
AkashMuraliNEW <akashmurali013@gmail.com>
2025-08-01 19:42:17 +0530

    math

--- programs/operations.cpp
+++ programs/operations.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int x=1, y=2;
 int main(){
     int sum = x+y;
     int multy = x*y;
-    cout << "sum of the number : " << sum << "\n multy of nums : " << multy ;
+    cout << "sum of the number : " << sum << "\nmulty of nums : " << multy <<'\n';
+    cout << sqrt(sum) << '\n';
+    cout << max(x,y) << '\n';
 
 }
