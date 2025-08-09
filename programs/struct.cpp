f3593f5926afa1af3b62cff50750b31a9ba0d2ba
AkashMuraliNEW <akashmurali013@gmail.com>
2025-08-09 18:05:58 +0530

    patternPrint

---
+++ programs/struct.cpp
@@ -0,0 +1,19 @@
+// #include <iostream>
+using namespace std;
+
+
+
+
+string akash = " hey ";
+
+int main(){
+//     struct myStruct{
+//     int size;
+// };
+//    myStruct.size;
+//    myStruct.size = 2;
+//     cout << myStruct.size <<"\n";
+    cout << akash;
+
+    return 0;
+}
